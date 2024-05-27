659b91233777b361e4d556add7e2606884193fae
Ead atividade <you@example.com>
2024-05-27 10:26:18 -0500

    atividade da platarforma de sp

--- atividadeCFE/js/script.js
+++ atividadeCFE/js/script.js
@@ -8,17 +8,17 @@ behavior: 'smooth'
 }
 //Validação de Login
 function login(){
-        var logado = 0;
-        var usuario  = document.getElementsByName('usuario')[0].value;
-        usuario = usuario.toLowerCase();
-        var senha = document.getElementById('senha').value;
-        senha = senha.toLowerCase();
+    var logado = 0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario = usuario.toLowerCase();
+    var senha = document.getElementById('senha').value;
+    senha = senha.toLowerCase();
     if(usuario == "admin" && senha == "123456"){
-        window.location == "index.html";
-        logado = 1;
+    window.location = "index.html";
+    logado = 1;
     }
-        if(logado == 0){
-            alert("Acesso Negado. Dados incorretos");
+    if(logado == 0){
+    alert("Acesso Negado. Dados incorretos");
     }
 }
 //Ativar alert no botão cadastrar
